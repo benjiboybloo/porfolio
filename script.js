a37e2a82de622caf0e6ca9adb34213824f5fd33e
benjiboybloo <britposy@gmail.com>
2024-12-15 17:23:51 -0500

    java retry
    this is hard and torturous but I am gonna get it!

---
+++ script.js
@@ -0,0 +1,31 @@
+//image slider
+
+const slides = document.querySelectorAll('.slides img');
+let slideIndex = 0;
+let intervalId =null;
+
+//initialize the slider
+
+document.addEventListener("DOMContentLoaded", initalizeSlider);
+
+
+functionin initalizeSlider() {
+    
+   if(slides.length > 0) {
+    slides[slideIndex].classList.add("displaySLide;");
+    intervalId = setInterval(nextSlide, 5000);
+}
+function showSlide(index){
+    slides.forEach(slide =>{
+        slide.classList.remove("displaySlide");
+    });
+    slides[slideIndex].classList.add("displaySlide");
+}
+function prevSlide({
+
+})
+
+function nextSlide() {
+    slideIndes++;
+    showSlide(slideIndex);
+}
