7f4113f44f2a39f7ab688b51edf01ef7443704d3
benjiboybloo <britposy@gmail.com>
2024-12-15 19:51:03 -0500

    FIXED IT
    I finally fixed the javascript

--- script.js
+++ script.js
@@ -10,22 +10,34 @@ document.addEventListener("DOMContentLoaded", initializeSlider);
 
 
 function initializeSlider() {
-    
    if(slides.length > 0) {
-    slides[slideIndex].classList.add("displaySLide;");
+    slides[slideIndex].classList.add("displaySlide");
     intervalId = setInterval(nextSlide, 5000);
+   }
 }
+
 function showSlide(index){
-    slides.forEach(slide =>{
+    if(index >= slides.length){
+        slideIndex = 0; 
+
+    } else if (index <0){
+        slideIndex= slides.length -1;
+    }
+
+    slides.forEach(slide => {
         slide.classList.remove("displaySlide");
     });
-    slides[slideIndex].classList.add("displaySlide");
+     slides[slideIndex].classList.add("displaySlide");
 }
-function prevSlide({
 
-})
+function prevSlide() {
+    clearInterval(intervalId);  
+    slideIndex--;
+    showSlide(slideIndex);  
+}
 
 function nextSlide() {
-    slideIndes++;
+    clearInterval(intervalId);
+    slideIndex++;
     showSlide(slideIndex);
 }
